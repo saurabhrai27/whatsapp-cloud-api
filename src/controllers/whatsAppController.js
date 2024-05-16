a1b53290d0f01e01f024335f2db4005069be06f6
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-05-17 01:31:26 +0530

    controllers to verify & receive messages throush webhook

--- src/controllers/whatsAppController.js
+++ src/controllers/whatsAppController.js
@@ -1,6 +1,33 @@
 import WhatsAppService from '../Services/WhatsAppService.js';
 const whatsAppService = new WhatsAppService();
 
+const verifyWebhook = async (req, res) => {
+    try {
+        if (
+            req.query['hub.mode'] == 'subscribe' &&
+            req.query['hub.verify_token'] == 'token'
+        ) {
+            res.send(req.query['hub.challenge']);
+        } else {
+            res.sendStatus(403);
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ status: false, errorMessage: error.message });
+    }
+}
+
+const receiveMessage = async (req, res) => {
+    try {
+        console.log(req.body);
+        console.log('Incoming webhook: ' + JSON.stringify(req.body));
+        res.sendStatus(200);
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ status: false, errorMessage: error.message });
+    }
+}
+
 const sendMessage = async (req, res) => {
     try {
         const response = await whatsAppService.sendMessage();
@@ -13,4 +40,6 @@ const sendMessage = async (req, res) => {
 
 export default {
     sendMessage,
+    receiveMessage,
+    verifyWebhook
 };
