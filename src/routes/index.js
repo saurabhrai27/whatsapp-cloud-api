a8ff7189b0a10ae7e96292a6f9443984d9b73892
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-05-17 01:34:12 +0530

    route for whatsapp webhook

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ import whatsAppController from '../controllers/whatsAppController.js';
 
 const router = express.Router();
 
+router.get('/webhook', whatsAppController.verifyWebhook); 
+router.post('/webhook', whatsAppController.receiveMessage);
 router.get('/sendMessage', whatsAppController.sendMessage);
 
 export default router;
